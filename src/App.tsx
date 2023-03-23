bc8b7e68e89358d3eed6a5c6698b82bf8abfe941
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-23 15:53:10 -0300

    Post fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
 import { Siderbar } from './components/Sidebar';
-import { Post } from './components/Post';
+import { Post, PostType } from './components/Post';
 
 import styles  from './App.module.css';
 
 import './global.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -53,9 +53,7 @@ function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
